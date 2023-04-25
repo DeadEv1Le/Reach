d4b36749b16541105079704a42ed198a75ed37e7
Tsogolakyan <tsogolakyan454@gmail.com>
2023-04-25 18:35:46 +0400

    post fragment change a little and adding user image, in profile page

--- app/src/main/java/org/tensorflow/lite/examples/detection/ui/user/UserFragment.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/ui/user/UserFragment.java
@@ -1,66 +1,158 @@
 package org.tensorflow.lite.examples.detection.ui.user;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import org.tensorflow.lite.examples.detection.R;
+import org.tensorflow.lite.examples.detection.databinding.FragmentUserBinding;
+import org.tensorflow.lite.examples.detection.ui.post.UploadActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UserFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class UserFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Uri uri;
+    AlertDialog dialog;
+    private FragmentUserBinding binding; // Add binding variable
+    private FirebaseStorage storage;
+    private DatabaseReference database;
 
     public UserFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UserFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UserFragment newInstance(String param1, String param2) {
-        UserFragment fragment = new UserFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false);
+
+        binding = FragmentUserBinding.inflate(inflater, container, false);
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        String uid = auth.getCurrentUser().getUid();
+
+        storage = FirebaseStorage.getInstance();
+        database = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+        androidx.appcompat.app.AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setCancelable(false);
+        builder.setView(R.layout.progress_layout);
+        dialog = builder.create();
+        dialog.show();
+
+
+        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK){
+                            Intent data = result.getData();
+                            uri = data.getData();
+                            binding.userProfileImage.setImageURI(uri);
+
+                            StorageReference storageRef = storage.getReference();
+                            StorageReference imageRef = storageRef.child("User images/" + uid + "/profile.jpg");
+                            imageRef.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                                    imageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                        @Override
+                                        public void onSuccess(Uri downloadUrl) {
+                                            // Save the download URL to the Firebase Realtime Database
+                                            database.child("profileImageUrl").setValue(downloadUrl.toString());
+                                        }
+                                    });
+                                }
+                            });
+                        } else {
+                            Toast.makeText(getContext().getApplicationContext(), "No Image Selected", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+
+        binding.userProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent photoPicker = new Intent(Intent.ACTION_PICK);
+                photoPicker.setType("image/*");
+                activityResultLauncher.launch(photoPicker);
+            }
+        });
+
+
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    // Get the user's email
+                    String email = dataSnapshot.child("email").getValue(String.class);
+                    binding.proffileEmail.setText(email);
+                    // Get the user's name
+                    String name = dataSnapshot.child("name").getValue(String.class);
+                    binding.profileUserCountry.setText(name);
+                    // Get the user's profile image URL
+                    String profileImageUrl = dataSnapshot.child("profileImageUrl").getValue(String.class);
+                    // Get the user's username
+                    if (profileImageUrl == null) {
+                        binding.userProfileImage.setImageResource(R.drawable.uploadimg);
+                    } else {
+                        Glide.with(requireContext())
+                                .load(profileImageUrl)
+                                .into(binding.userProfileImage);
+                    }
+                    String username = dataSnapshot.child("username").getValue(String.class);
+                    binding.profileUserName.setText(username);
+                    // Do something with the retrieved data
+                    // ...
+                    dialog.dismiss();
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle errors here
+            }
+        });
+
+
+        return binding.getRoot();
     }
 }
+
+
